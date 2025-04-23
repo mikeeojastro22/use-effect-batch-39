f38d28566bd716e04f149b5a4b4fdbdae18513e2
Mikee Ojastro <mikeeojastro@gmail.com>
2025-04-23 19:04:33 +0800

    April 23, 2025

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  function fetchUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    // We get the response from the API in JSON
+    .then(response => response.json())
+    // Setting user array
+    .then(data => setUsers(data))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchUsers}>Click me!</button>
+      {
+        users.map(user => {
+          const {id, name, email} = user;
+          return (
+            <div key={id}>
+              <p>Name: {name}</p>
+              <p>Email: {email}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
